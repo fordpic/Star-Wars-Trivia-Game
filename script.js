aa8d8777c433bf03a8db03800a0abe1f6f015c8c
fordpic <fordpickert@gmail.com>
2021-02-21 13:32:52 -0600

    getting global variables down

---
+++ script.js
@@ -0,0 +1,14 @@
+let playerAnswer = [];
+const questions = [
+	"Who was Luke Skywalker's first master? Was it: A. Obi-Wan Kenobi B. Gandhi C. Anakin Skywalker D. Mace Windu",
+	"In his first vicious & downright CGI duel with Count Dooku, which limb did Anakin Skywalker lose? Was it: A. His left leg B. The toes on his right foot C. His right arm D. His left ear",
+	'On what planet did the Clone Wars begin? A. Naboo B. Tatooine C. Uranus D. Geonosis',
+	"What world class, dashing young actor played the near invincible Qui-Gon Jinn in the visually stunning Episode 1? Was it: A. Tom Holland B. Liam Neeson C. Ford Pickert D. Sarah Jessica Parker",
+	"What was the name of the most infamous ship in the galaxy? (Hint: It was piloted by Indiana Jones & Bigfoot) A. Starship STX-90 B. Copper Cougar C. Miellenium Falcon D. Lost Ark",
+    "Which character absolutely annihilates the Death Star? A. George Lucas B. Han Solo C. Princess Leia D. Luke Skywalker",
+    "What species of humanoids does Chewbacca belong to? A. Wookie B. Cookie C. Snookie D. Sasquatch",
+    "How far are we really from all this goodness? A. Far away B. Far, far away C. Like two million miles D. Really close actually",
+    "Han Solo has a weirdo wannabe Sith kid with which iconic character (and actress!)? A. R2-D2 B. Your mom C. Princess Leia D. Queen Padme Amidala",
+    "Complete this iconic line: 'Luke, I am your ___.' A. Dude B. Dad C. Parental Unit D. Father"
+];
+
